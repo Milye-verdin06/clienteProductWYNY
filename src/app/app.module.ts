9ac6128dad8a18fd4ed870b86faa92687f86a1e0
Milye-verdin06 <milagros.espinosa.verdin@gmail.com>
2021-03-10 12:20:18 -0600

    html adjuntado de componentes: navbar,sidebar,home,footer. falta el style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HomeComponent } from './components/home/home.component';
 import { ProductoComponent } from './models/producto/producto.component';
 import { ClienteComponent } from './models/cliente/cliente.component';
 import { PerfilvendedorComponent } from './models/perfilvendedor/perfilvendedor.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { PerfilvendedorComponent } from './models/perfilvendedor/perfilvendedor.
     HomeComponent,
     ProductoComponent,
     ClienteComponent,
-    PerfilvendedorComponent
+    PerfilvendedorComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
